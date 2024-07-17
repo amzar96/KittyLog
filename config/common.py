3b50f2d6999822d7ea352439e34d5b0a157eea70
amzar <mohdamzar96@gmail.com>
2024-07-18 01:15:48 +0800

    chore: added initial files chore: added db migration folder structure

---
+++ config/common.py
@@ -0,0 +1,22 @@
+import os
+import configparser
+from config import cfg as CFG
+
+
+def home_dir():
+    return os.getcwd()
+
+
+def get_database_url():
+    return f"postgresql+psycopg2://{CFG.DB_USER}:{CFG.DB_PASSWORD}@{CFG.DB_HOST}:{CFG.DB_PORT}/{CFG.DB_NAME}"
+
+
+def update_alembic_config():
+    config_file_path = f"{home_dir()}/db/alembic.ini"
+    config = configparser.ConfigParser()
+    config.read(config_file_path)
+
+    config["alembic"]["sqlalchemy.url"] = get_database_url()
+
+    with open(config_file_path, "w") as configfile:
+        config.write(configfile)
