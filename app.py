61f32ff56ad97773311ed0f95d568a8b1bf1eeb1
amzar <mohdamzar96@gmail.com>
2024-10-27 17:05:04 +0800

    added htmls and google sign in

--- app.py
+++ app.py
@@ -2,10 +2,18 @@ import logging
 from core import models
 from fastapi import FastAPI
 from database.db import engine
+from config import cfg as CFG
 from routers import users, cats
-
+from starlette.requests import Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from starlette.responses import RedirectResponse
+from starlette.middleware.sessions import SessionMiddleware
+from authlib.integrations.starlette_client import OAuth, OAuthError
 
 models.Base.metadata.create_all(bind=engine)
+
+templates = Jinja2Templates(directory="templates")
 
 app = FastAPI(
     title="KittyLog",
@@ -17,6 +25,21 @@ app = FastAPI(
 app.include_router(users.router)
 app.include_router(cats.router)
 
+app.add_middleware(SessionMiddleware, secret_key="ajajsyhhd")
+# app.mount("/static", StaticFiles(directory="static"))
+
+oauth = OAuth()
+oauth.register(
+    name="google",
+    server_metadata_url="https://accounts.google.com/.well-known/openid-configuration",
+    client_id=CFG.CLIENT_ID,
+    client_secret=CFG.CLIENT_SECRET,
+    client_kwargs={
+        "scope": "email openid profile",
+        "redirect_url": "https://localhost:8000/auth",
+    },
+)
+
 logging.config.fileConfig("logging.conf", disable_existing_loggers=False)
 logger = logging.getLogger(__name__)
 
@@ -24,6 +47,48 @@ logger = logging.getLogger(__name__)
 @app.get("/health")
 async def root():
     return {"message": "server is up"}
+
+
+@app.get("/")
+def login(request: Request):
+    user = request.session.get("user")
+
+    if user:
+        return RedirectResponse("home")
+
+    return templates.TemplateResponse("home.html", {"request": request})
+
+
+@app.get("/login")
+async def login(request: Request):
+    url = request.url_for("auth")
+    return await oauth.google.authorize_redirect(request, url)
+
+
+@app.get("/auth")
+async def auth(request: Request):
+    try:
+        token = await oauth.google.authorize_access_token(request)
+    except OAuthError as e:
+        return templates.TemplateResponse("error.html", {"request": request})
+
+    user = token.get("userinfo")
+
+    if user:
+        request.session["user"] = dict(user)
+
+    return RedirectResponse("home")
+
+
+@app.get("/home")
+async def home(request: Request):
+    user = request.session.get("user")
+
+    if not user:
+        RedirectResponse("/")
+    return templates.TemplateResponse(
+        "home.html", {"request": request, "user": user}
+    )
 
 
 if __name__ == "__main__":
--- config/cfg.py
+++ config/cfg.py
@@ -9,3 +9,7 @@ DB_PASSWORD = os.environ.get("DB_PASSWORD")
 DB_NAME = os.environ.get("DB_NAME")
 DB_HOST = os.environ.get("DB_HOST")
 DB_PORT = os.environ.get("DB_PORT")
+
+# google auth
+CLIENT_ID = os.environ.get("CLIENT_ID")
+CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
